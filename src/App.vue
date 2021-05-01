73d77a8bc721f122bfdf2639482b9151d538a9aa
Simon Mettler <admin@simonmettler.com>
2021-05-01 12:01:53 +0200

    remove fhgr logo from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
 
-    <img alt="Vue logo" src="./assets/fhgr.jpg">
     <HelloWorld v-if="show=='methodencheck'" msg="Hallo methodencheck!"/>
     <Bibeval v-if="show=='bibeval'"/>
     <HelloWorld v-if="show=='suchanfrage'" msg="Hallo suchanfrage!"/>
-    {{show}}
+
   </div>
 </template>
 
