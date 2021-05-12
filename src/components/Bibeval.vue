557ce029a8943f7bfb4f79a674edab6b28cbe7d4
nino <nino.parolari@gmail.com>
2021-05-12 16:26:57 +0200

    bibeval question erweitert

--- src/components/Bibeval.vue
+++ src/components/Bibeval.vue
@@ -1,18 +1,112 @@
 <template>
-  <div class="hello">
-    <question></question>
+  <div class="">
+    <!-- Beispiel: mehrere Question-Komponenten anzeigen -->
+    <table>
+      <tr>
+        <th class="question-col">Frage</th>
+        <th>gar nicht</th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th>absolut</th>
+        <th>k.A.</th>
+      </tr>
+      <question
+      v-for="(q, index) in questions"
+      :question="q"
+      :key="q.uid"
+      v-model="answers[index]"
+    ></question>
+    </table>
+
+    {{ answers }}
   </div>
 </template>
 
 <script>
-import question from './question.vue'
+import question from "./question.vue";
+
+let data = [
+  {
+    uid: "1",
+    pid: "754",
+    tstamp: "1353072831",
+    lmuser_id: "103",
+    crdate: "1345708532",
+    cruser_id: "103",
+    deleted: "0",
+    hidden: "0",
+    category_id: "2",
+    name_de: '"Sind die Informationsseiten klar und deutlich strukturiert',
+    '"name_en"': ' um die Anwender bei der Orientierung zu unterstützen?"',
+    '"desc_de"':
+      '"Is the information section well-structured assisting the user in his orientation?"',
+    '"desc_en"': '"Es wird bspw. mit Überschriften unterschiedlichen Grades',
+    is_mandatory: " Listen",
+    order_number:
+      ' etc. gearbeitet. Bei längeren Texten bieten sich Textmarken (\\"Anker\\") an',
+  },
+  {
+    uid: "2",
+    pid: "754",
+    tstamp: "1353073026",
+    lmuser_id: "103",
+    crdate: "1345708532",
+    cruser_id: "103",
+    deleted: "0",
+    hidden: "0",
+    category_id: "2",
+    name_de:
+      '"Beschränken sich die Informationstexte auf das Wesentliche und wird für diese eine leicht verständliche Sprache sowie ein einfacher Satzbau verwendet?"',
+    '"name_en"':
+      '"Does the information provided confine itself to the essentials and are the documents easy to follow (e.g. a simple writing style should be used)?"',
+    '"desc_de"':
+      '"Überflüssige Informationen und Fachwörter werden vermieden."',
+    '"desc_en"': '"Unnecessary information and technical terms are avoided."',
+    is_mandatory: "1",
+    order_number: "2",
+  },
+  {
+    uid: "3",
+    pid: "754",
+    tstamp: "1353073026",
+    lmuser_id: "103",
+    crdate: "1345708532",
+    cruser_id: "103",
+    deleted: "0",
+    hidden: "0",
+    category_id: "2",
+    '"name_de"':
+      '"Beschränken sich die Informationstexte auf das Wesentliche und wird für diese eine leicht verständliche Sprache sowie ein einfacher Satzbau verwendet?"',
+    '"name_en"':
+      '"Does the information provided confine itself to the essentials and are the documents easy to follow (e.g. a simple writing style should be used)?"',
+    '"desc_de"':
+      '"Überflüssige Informationen und Fachwörter werden vermieden."',
+    '"desc_en"': '"Unnecessary information and technical terms are avoided."',
+    is_mandatory: "1",
+    order_number: "2",
+  },
+];
 
 export default {
-  name: 'Bibeval',
-  components: {
-    question
+  name: "Bibeval",
+  data: function () {
+    let answers = [];
+
+    for (let i = 0; i < data.length; i++) {
+      answers.push(0);
     }
-}
+
+    return {
+      answers: answers,
+      // Zum Testen mal zwei Beispielfragen, TODO aktuelles JSON-Format verwenden
+      questions: data,
+    };
+  },
+  components: {
+    question,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -30,5 +124,18 @@ li {
 }
 a {
   color: #42b983;
+}
+table{
+  text-align: left;
+  border-collapse: collapse;
+  border:1px solid black;
+}
+td, th{
+  width:100px;
+  padding: 1em 0em;
+  border:1px solid black;
+}
+.question-col{
+  width:500px;
 }
 </style>
--- src/components/question.vue
+++ src/components/question.vue
@@ -1,13 +1,90 @@
 <template>
-  <div class="">
-    <h1>Frage X</h1>
-  </div>
+  <tr class="single-question">
+    <!-- auf Details einer Fragee zugreifen -->
+    <td class="question-col">{{ question.name_de }}</td>
+    <td>
+      <input
+      type="radio"
+      id="zero"
+      value="0"
+      :name="question.uid"
+      v-model="picked"
+    />
+    <!-- <label for="one">One</label> -->
+    </td>
+    <td>
+      <input
+        type="radio"
+        id="one"
+        value="1"
+        :name="question.uid"
+        v-model="picked"
+      />
+    
+    <!-- <label for="two">Two</label> -->
+    </td>
+    <td>
+      <input
+        type="radio"
+        id="two"
+        value="2"
+        :name="question.uid"
+        v-model="picked"
+      />
+    </td>
+    <td>
+      <input
+        type="radio"
+        id="three"
+        value="3"
+        :name="question.uid"
+        v-model="picked"
+      />
+    </td>
+    <td>
+      <input
+        type="radio"
+        id="four"
+        value="4"
+        :name="question.uid"
+        v-model="picked"
+      />
+    </td>
+    <td>
+      <input
+        type="radio"
+        id="five"
+        value="5"
+        :name="question.uid"
+        v-model="picked"
+      />
+    </td>
+    <td>
+      <span>Picked: {{ picked }}</span>
+    </td>
+  </tr>
 </template>
 
 <script>
 export default {
-  name: 'question',
-}
+  name: "question",
+  // als Prop: JSON einer einzelnen Frage wird übergeben
+  props: ["question", "value"],
+  data: function () {
+    return {
+      picked: this.value,
+    };
+  },
+  watch: {
+    picked: function (neuerWert) {
+      this.$emit("input", neuerWert);
+    },
+
+    /*updateValue: function (value) {
+      this.$emit("input", value);
+    },*/
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -25,5 +102,10 @@ li {
 }
 a {
   color: #42b983;
+}
+td{
+  padding: 1em 0em;
+  text-align:center;
+  border:1px solid black;
 }
 </style>
