315d5d3c33da4789ee0789a6c08bcd26c7da7c16
Simon Mettler <admin@simonmettler.com>
2021-05-15 17:33:07 +0200

    load subcategories as a nested array

--- src/components/Bibeval.vue
+++ src/components/Bibeval.vue
@@ -3,8 +3,9 @@
 
     <question></question>
 
-		<h2>Untersuchungsbereich?</h2>
+		<h1>Untersuchungsbereich?</h1>
 
+		<!-- Loop over category level one. -->
 		<div v-for="cat1 in categories" :key="cat1">
 
 			<select-button 
@@ -14,7 +15,7 @@
 			>{{ cat1[0] }} 
 			</select-button>
 
-
+			<!-- Loop over category level two. -->
 			<template v-if="selectedCategories.includes(cat1[0])">
 				<div v-for="cat2 in cat1[1]" :key="cat2">
 
@@ -30,21 +31,21 @@
 
 	</div>
 
-<!-- 
-
     <div v-for="subcategory in selectedSubCategories" :key="subcategory">
-      <h1>{{ subcategory }}</h1>
+      <h2>{{ subcategory }}</h2>
 
-      <button v-for="comp in getComponents(subcategory)" :key="comp">
+      <select-button v-for="comp in getComponents(subcategory)" :key="comp" :value="comp" v-model="selectedComponents">
         {{ comp }}
-      </button>
+      </select-button>
+
     </div>
-		-->
 
-{{ selectedCategories }}<br>
-{{ selectedSubCategories }}<br>
-{{ data_bibeval}}
-
+		<!-- DEV -->
+		Bereiche: {{ selectedCategories }}<br>
+		Teilbereiche: {{ selectedSubCategories }}<br>
+		Komponenten: {{ selectedComponents }}<br>
+		{{ categories }}<br>
+		{{ subcategories }}
   </div>
 </template>
 
@@ -52,15 +53,18 @@
 import question from "./question.vue";
 import selectButton from "./SelectButton.vue";
 
-import bibeval_json from './json/data_bibeval.json';
+import bibeval_json from "./json/data_bibeval.json";
 
 export default {
   name: "Bibeval",
+
   data () {
     return {
       selectedCategories: [],
       selectedSubCategories: [],
+      selectedComponents: [],
 			categories: [],
+			subcategories: [],
 			data_bibeval: bibeval_json,
     };
   },
@@ -72,19 +76,39 @@ export default {
 
   methods: {
     getComponents(subcategory) {
-      console.log(subcategory);
-      return ["a", "b", "c"];
-    },
-  },
-	mounted() {
-			var categories = [];
-			for(var i = 0; i < this.data_bibeval.categories_levelone.length; i++) {
-				categories.push([this.data_bibeval.categories_levelone[i].name]);
-				for(var x = 0; x < this.data_bibeval.categories_levelone[i].categories_leveltwo.length; x++) {
-					categories[i].push([this.data_bibeval.categories_levelone[i].categories_leveltwo[x].name]);
+			for(var i = 0; i < this.subcategories.length; i++) {
+				if( this.subcategories[i][0] === subcategory ){
+					return this.subcategories[i][1];
 				}
 			}
-			this.categories = categories;
+    },
+  },
+
+	mounted() {
+
+		// Load categories as a nested array.
+		var categories = [];
+		for(var i = 0; i < this.data_bibeval.categories_levelone.length; i++) {
+			categories.push([this.data_bibeval.categories_levelone[i].name]);
+			for(var x = 0; x < this.data_bibeval.categories_levelone[i].categories_leveltwo.length; x++) {
+				categories[i].push([this.data_bibeval.categories_levelone[i].categories_leveltwo[x].name]);
+			}
+		}
+		this.categories = categories;
+
+
+		// Load subcategories (Komponenten) as a nested array.
+		var subcategories = [];
+		for(var y = 0; y < this.data_bibeval.categories_levelone.length; y++) {
+			for(var z = 0; z < this.data_bibeval.categories_levelone[y].categories_leveltwo.length; z++) {
+				subcategories.push([this.data_bibeval.categories_levelone[y].categories_leveltwo[z].name, []]);
+				for(var a = 0; a < this.data_bibeval.categories_levelone[y].categories_leveltwo[z].categories_levelthree.length; a++) {
+					subcategories[y][1].push(this.data_bibeval.categories_levelone[y].categories_leveltwo[z].categories_levelthree[a].name)
+				}
+			}
+		}
+		this.subcategories = subcategories; 
+
 	}
 };
 </script>
