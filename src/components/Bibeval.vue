df560d9fcd5405efd9f43ed00aef199710153bcc
nino <nino.parolari@gmail.com>
2021-06-02 16:37:11 +0200

    kleine ergaenzung IMG

--- src/components/Bibeval.vue
+++ src/components/Bibeval.vue
@@ -2,7 +2,7 @@
     <div class="eval-content" id="bibeval-top">
         <!-- PAGE 0 / INFO PAGE -->
         <template v-if="page == 0">
-          <img class="bib-header-img" src="../assets/bibeval_intro_image.png" />
+          <img class="bib-header-img" src="/apps/stand1105/dist/img/bibeval_intro_image.2d5e403a.png" />
           <!-- LANGUAGE SWITCH -->
           <div class="eval-languageswitch">
               <button class="linkbutton" v-on:click="language = 'de'; loadLabels()" v-bind:class="{linkbuttonActive: language == 'de'}">
